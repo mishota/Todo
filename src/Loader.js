34b74f22f584e8b8e2967aace954f21917b39041
Oksana Mishota <ksu_a@mail.ru>
2021-06-29 13:15:33 +0300

    Initial commit

---
+++ src/Loader.js
@@ -0,0 +1,10 @@
+import { findByLabelText } from '@testing-library/react'
+import React from 'react'
+
+export default () => ( 
+<div stile={{display: 'flex', justifycontent: 'center', margin: '.5rem'}}>
+   <div className="lds-dual-ring"/>
+</div> 
+)
+   
+   
